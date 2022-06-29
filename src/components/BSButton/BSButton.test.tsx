0834bac00cb499593ec6373e2787389c7dfeab9e
Nicolas Agustín Perez <nicolasperez95.a@gmail.com>
2022-06-29 05:31:32 -0300

    feat: added bsbutton

--- src/components/BSButton/BSButton.test.tsx
+++ src/components/BSButton/BSButton.test.tsx
@@ -6,14 +6,42 @@ import { BSButtonProps } from './BSButton.types';
 const defaultProps: BSButtonProps = {
   children: 'Button',
   onClick: jest.fn(),
-}
+};
 
 describe('Boostrap Button Comoponent', () => {
   it('Should render with only mandatory props', () => {
-    
-    const { getByText, getByRole } = render(<BSButton {...defaultProps} />);
+    const { getByText } = render(<BSButton {...defaultProps} />);
     const component = getByText('Button');
-    
+
     expect(component).toBeTruthy();
-  })
+    component.click();
+    expect(defaultProps.onClick).toBeCalled();
+  });
+
+  it('Should not be called onClick on disabled button', () => {
+    const clickFunction = jest.fn();
+    const props: BSButtonProps = { ...defaultProps, onClick: clickFunction, disabled: true };
+
+    const { getByText } = render(<BSButton {...props} />);
+    const component = getByText('Button');
+    component.click();
+
+    expect(clickFunction).toBeCalledTimes(0);
+  });
+
+  it('Should be render with correct classnames & role', () => {
+    const props: BSButtonProps = {
+      ...defaultProps,
+      styleType: 'success',
+      className: 'test',
+      size: 'lg',
+      isOutline: true,
+      role: 'reset',
+    };
+
+    const { getByText } = render(<BSButton {...props} />);
+    const component = getByText('Button'); 
+
+    expect(component).toHaveClass('btn-lg btn btn-outline-success test');
+  });
 });
